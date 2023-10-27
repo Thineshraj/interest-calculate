5cc50f2e1bb41f6927e2ae4c7a25a06313a44ec2
Thineshraj S <63834782+Thineshraj@users.noreply.github.com>
2023-10-27 12:26:08 -0400

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -2,8 +2,8 @@
 # This script calculates simple interest given principal, annual rate of interest and time period in years.
 # Do not use this in production. Sample purpose only.
 
-# Author: Thineshraj Soosaiselvarajah
-# Addtional Authors:
+# Author: Upkar Lidder (IBM)
+# Addtional Authors: Thineshraj Soosaiselvarajah
 # Thineshraj
 
 # Input:
